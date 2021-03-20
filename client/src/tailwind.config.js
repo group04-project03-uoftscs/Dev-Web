ad4f388caabe7417f19be694987a517c38147721
Annie Kwan <akwan.mtl@gmail.com>
2021-03-20 17:43:07 -0400

    testing out tailwind.config.js

--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  purge: [
-    './public/**/*.html',
-    './src/pages/*.{js,jsx,ts,tsx,vue}',
-    './src/components/**/*.{js,jsx,ts,tsx,vue}',
-  ],
+  purge: {
+      content:[
+      './public/**/*.html',
+      './src/pages/*.{js,jsx,ts,tsx,vue}',
+      './src/components/**/*.{js,jsx,ts,tsx,vue}',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
