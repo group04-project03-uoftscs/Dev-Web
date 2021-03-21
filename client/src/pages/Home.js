6e6ccf65c8492a9c226f78c239caf720e7378548
Annie Kwan <akwan.mtl@gmail.com>
2021-03-21 18:59:06 -0400

    BIG CHANGES
    added private routes for preventing users from going to different routes
    
    changed home.js to render landing page or dashboard based on state.logged

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,6 +10,9 @@ import { AUTH_METHOD, FOUND_USER, LOADED, LOADING, UPDATE_FAVORITES, UPDATE_LOCA
 import { useHistory } from 'react-router-dom';
 import API from '../utils/API';
 
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+
 function Home () {
   const [offsetY, setOffsetY] = useState(0);
   const handleScroll = () => setOffsetY(window.pageYOffset);
@@ -81,7 +84,7 @@ function Home () {
         dispatch({
           type: LOADED
         })
-        history.push('/landing')
+        // history.push('/landing')
       }
     }
     getUser();
@@ -149,63 +152,10 @@ function Home () {
 
   console.log(state)
 
-  const renderContent = () => (
-    <>
-      <div className="mx-14 flex flex-col justify-center max-h-full pl-2 pr-2 rounded-md xl:pr-4 gap-y-36">
-        
-        <div className="p-4 text-white bg-blue-500 rounded-md shadow-md">
-              <div className="flex items-center justify-center">
-                <span className="text-3xl font-semibold tracking-wider uppercase">Welcome {state.user.username}!</span>
-              </div>
-            </div>
-        { (Object.keys(state.user).length !== 0 && Object.keys(state.codewars).length !== 0) ? 
-          <div>
-            User: {state.user.username}
-            <br />
-            Code: {state.codewars.name}
-          </div>
-          :
-          <div>
-            <div className="flex items-center w-1/2 justify-center p-4 mt-4 bg-white rounded-md shadow-md">
-              <span className="text-xl tracking-wider text-gray-500 uppercase">Loading user account info</span>
-            </div> 
-          </div>
-        }
-
-        <div>
-          {state.favorites.length!==0 ?  state.favorites.slice(0,Math.min(state.favorites.length,5)).map((article) => {
-            return (
-              <Card article={article} key={article.id}/>
-            )
-          })
-          : 
-          <div>
-            <div className="flex items-center w-1/2 justify-center p-4 mt-4 bg-white rounded-md shadow-md">
-              <span className="text-xl tracking-wider text-gray-500 uppercase">No Bookmarks</span>
-            </div>
-          </div>
-        }
-        </div>
-      </div>
-    </>
-  );
-
-  return(
-    <div>
-      {console.log(state)}
-      <section className="Parallax">
-        <div
-          className="Parallax__background"
-          style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
-        />
-        <div
-          className="Parallax__background-triangles"
-          style={{ transform: `translateY(${offsetY * 0.8}px)` }}
-        />
-        <div className="w-full Parallax__content">{renderContent()}</div>
-      </section>
-    </div>
-  )
+  if(state.logged) return <Dashboard/>
+  else return <Landing/>
+ 
+  
 }
 
 export default Home;
