831ab2b57d8d2bcaa90bc2b615a054313600233a
Jhonny Lin <jhonnylin72@gmail.com>
2021-03-23 20:58:10 -0400

    Added the email field

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String, unique: true},
+  username: { type: String, unique: true },
+  email: { type: String, uniquie: true },
   password: { type: String },
   github: { type: Object, unique: true },
   auth: { type: String},
