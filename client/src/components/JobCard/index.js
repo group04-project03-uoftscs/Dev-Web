0d06ba2b6d6df9b515d13f82d38f1d9943725436
Niv Suroghon <nsuroghon@gmail.com>
2021-03-27 10:52:56 -0400

    removing comments

--- client/src/components/JobCard/index.js
+++ client/src/components/JobCard/index.js
@@ -87,25 +87,6 @@ const Errorpic = 'https://i.postimg.cc/fWdKWTTV/Dev-Web.gif';
         </div>
       </div>
   )
-  // return (
-  //   <div className="job-card" index={index + 1} style={{ marginLeft:"15px"}}>
-  //     <div className="company-logo">
-  //       <img src={company_logo} alt={company} width="100" height="100"/>
-  //     </div>
-  //     <div className="job-info">
-  //       <div className="job-title">{title}</div>
-  //       <div className="job-location">
-  //         {location} | {type}
-  //       </div>
-  //       <div className="company-name">{company}</div>
-  //     </div>
-  //     <div className="post-info">
-  //       <div className="post-time">
-  //         Posted {moment(new Date(created_at)).fromNow()}
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default JobCard;
