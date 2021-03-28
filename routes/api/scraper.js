e63c5ffa6e76a429ca0ee8885b8328310bf491b1
Annie Kwan <akwan.mtl@gmail.com>
2021-03-28 13:11:40 -0400

    added arguments for heroku

--- routes/api/scraper.js
+++ routes/api/scraper.js
@@ -10,23 +10,25 @@ const scrape = async (scrapeURL) => {
             "--incognito",
             "--no-sandbox",
             "--single-process",
-            "--no-zygote",
+            "--no-zygote", 
+            '--disable-setuid-sandbox'
         ],
+        'ignoreHTTPSErrors': true
     };
     let image = "";
 
     const browser = await puppeteer.launch(chromeOptions);
     const page = await browser.newPage();
     await page.setRequestInterception(true)
+    await page.goto(scrapeURL, {waitUntil: 'networkidle2'});
+    page.on('request', (request) => {
+        if (request.resourceType() === 'document') {
+            request.continue();
+        } else {
+            request.abort();
+        }
+    });
     try{
-      await page.goto(scrapeURL, {waitUntil: 'networkidle2'});
-      page.on('request', (request) => {
-          if (request.resourceType() === 'document') {
-              request.continue();
-          } else {
-              request.abort();
-          }
-      });
       image = await page.$eval('meta[property="og:image"]', el => el.content);
     }
 
