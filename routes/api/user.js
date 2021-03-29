708d7383ba34d2d4e37e969d439436e6ced3b282
Annie Kwan <akwan.mtl@gmail.com>
2021-03-29 19:59:44 -0400

    removed console.log in userController.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
 // Matches with "/api/user"
-
 router
   .route("/")
   .get(userController.getAllGithubUsers)
